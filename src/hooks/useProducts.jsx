90523985ff746451100133f11169ffd37f10af6e
Cecilia Mary <ceciliamary78@gmail.com>
2024-06-27 21:47:34 -0300

    Firebase1

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import { getAllProducts } from "../services/products"; 
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
 
 export const useProducts = () => {
+  const [loading, setLoading] = React.useState(true);
   const [products, setProducts] = React.useState([]);
+  
   React.useEffect(() => {
-    getAllProducts()
-    .then((res) => {
-        setProducts(res.data.products);
+    const db = getFirestore();
+
+    const productsCollection = collection(db, "products");
+    getDocs(productsCollection)
+    .then((snapshot) => {
+      setProducts(
+      snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+    );
     })
-    .catch((error) => {
-        console.error(error);
-    });
+    .catch((error) => console.log(error)) 
+    .finally(() => setLoading(false));
+    
   }, []);
   return { products };
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { initializeApp } from "firebase/app";
 import App from './App.jsx'
 import './index.css'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChU4blvuMwI-v2K_EJIFHWHGGic88cZik",
+  authDomain: "cecilia-mary-react.firebaseapp.com",
+  projectId: "cecilia-mary-react",
+  storageBucket: "cecilia-mary-react.appspot.com",
+  messagingSenderId: "96752064517",
+  appId: "1:96752064517:web:e549e37b0cbb48cfe8e46c"
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
